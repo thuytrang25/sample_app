2ab31e568870b619edc345ef3b4027c81aefe571
Thuy Trang <thainguyenthuytrang1041999@gmail.com>
2020-04-25 02:04:26 +0700

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,11 +4,11 @@ require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors, with: :threads)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+	fixtures :all
+# Returns true if a test user is logged in.
+def is_logged_in?
+	!session[:user_id].nil?
+end
 
   # Add more helper methods to be used by all tests here...
 end
